c6bd5ea0cdc9e93ccdee7e222ce94222ae8dbfc9
MrSidSir <irshad1554@gmail.com>
2025-06-15 04:04:08 +0530

    Initial commit: React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-xl text-center mt-10">
+      Tailwind CSS is working!
+    </div>
+  );
+};
+
+export default App;
